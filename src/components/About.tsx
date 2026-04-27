6df56447274551d787de70dac868179a8c3288e4
Ilavbare Adeola <raisingsrs@gmail.com>
2026-04-27 17:31:04 +0100

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -87,7 +87,7 @@ export default function About() {
                 className="rounded-2xl w-full h-48 object-cover object-top shadow-lg -mt-4"
               />
               <img
-                src="/group.PNG"
+                src="/group.JPG"
                 alt="Children playing"
                 className="rounded-2xl w-full h-48 object-cover shadow-lg mt-4"
               />
