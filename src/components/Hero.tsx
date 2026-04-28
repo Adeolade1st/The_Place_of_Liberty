0aaa119b9116d74b75876e7e09466d6f5ecdc181
Ilavbare Adeola <raisingsrs@gmail.com>
2026-04-28 21:30:07 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ export default function Hero() {
   };
 
    return (
-    <section id="home" className="relative h-screen min-h-[600px] overflow-hidden bg-zinc-1400">
+    <section id="home" className="relative h-screen min-h-[600px] overflow-hidden bg-zinc-800">
       {/* Background Slides */}
       {slides.map((slide, index) => (
         <div
