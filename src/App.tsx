9b06cfa83ebdf2ef058b1db0219e8ea290f20251
Ilavbare Adeola <raisingsrs@gmail.com>
2026-05-08 11:31:43 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,10 +77,7 @@ export default function App() {
         {currentPage === 'history' && <HistoryPage />}
         {currentPage === 'contact' && <ContactPage />}
         {currentPage === 'who-about' && (
-          <>
-            <About />
-            <div className="pt-20"><VisionMission /></div>
-          </>
+
         )}
         {currentPage === 'offer-special-ed' && <SpecialEdPage />}
         {currentPage === 'offer-vocational' && <VocationalOfferPage />}
