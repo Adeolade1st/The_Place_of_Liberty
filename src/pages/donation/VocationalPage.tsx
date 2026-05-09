4c594e079338166727195b4500fe7c8ebfb34f1d
Ilavbare Adeola <raisingsrs@gmail.com>
2026-05-09 07:46:39 +0100

    Updated VocationalPage.tsx

--- src/pages/donation/VocationalPage.tsx
+++ src/pages/donation/VocationalPage.tsx
@@ -59,7 +59,7 @@ export default function VocationalPage() {
           </div>
           <div>
             <img
-              src='/creatives.jpg'
+              src='/mopmaking.webp'
               alt="Vocational training"
               className="rounded-2xl w-full h-72 object-cover shadow-lg mb-6"
             />
