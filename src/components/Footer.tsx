7ad0df655d3fab6251bdc97ee25567d548ef37e9
Ilavbare Adeola <raisingsrs@gmail.com>
2026-05-09 07:09:50 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -146,7 +146,7 @@ export default function Footer({ onNavigate }: FooterProps) {
             <li className="flex items-center gap-3">
               <Mail size={16} className="text-orange-500 flex-shrink-0" />
               <a href="mailto:info@theplaceofiberty.edu.ng" className="text-gray-400 hover:text-orange-400 text-sm transition-colors break-all">
-                example@pol.edu.ng
+                info@placeofliberty.com
               </a>
             </li>
           </ul>
