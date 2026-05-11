cc9bbe1ed0780348556a064e42b378415c82a889
Ilavbare Adeola <raisingsrs@gmail.com>
2026-05-11 11:03:03 +0100

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -92,7 +92,7 @@ export default function About() {
                 className="rounded-2xl w-full h-48 object-cover object-top shadow-lg -mt-4"
               />
               <img
-                src="/proupimg.webp"
+                src="/game.webp"
                 alt="group photo"
                 className="rounded-2xl w-full h-48 object-cover shadow-lg mt-4"
               />
